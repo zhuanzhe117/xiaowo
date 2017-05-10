07626a066733fcd7473c000dd412f011dbba3b7c
gu.haiyan <gu.haiyan@jyall.com>
2017-05-10 10:14:23 +0800

    建项

---
+++ xiaowo-structure/structure-eureka/src/main/java/EurekaApp.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+/* =======================================================
+ * 小窝
+ * 日 期：10/17/16
+ * 作 者：Hannah
+ * 版 本：0.0.1
+ * 描 述：注册中心启动类
+ * ========================================================
+ */
+@EnableEurekaServer
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.xiaowo"})
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class,args);
+//        new SpringApplicationBuilder(EurekaApp.class).web(true).run(args);
+        System.out.println("启动成功");
+    }
+}
